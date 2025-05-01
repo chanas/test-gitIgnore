e51702f16b2c7c800897cc742cbf678f76174181
Chris Hanas <chanas@ctemc.org>
2025-05-01 12:52:46 -0400

    Test gitignore file

---
+++ test-1/src/main.py
@@ -0,0 +1,19 @@
+# ---------------------------------------------------------------------------- #
+#                                                                              #
+# 	Module:       main.py                                                      #
+# 	Author:       cphan                                                        #
+# 	Created:      5/1/2025, 12:52:15 PM                                        #
+# 	Description:  V5 project                                                   #
+#                                                                              #
+# ---------------------------------------------------------------------------- #
+
+# Library imports
+from vex import *
+
+# Brain should be defined by default
+brain=Brain()
+
+brain.screen.print("Hello V5")
+
+
+        
